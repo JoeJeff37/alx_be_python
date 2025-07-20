701c033311bdd019161443e1819e4a86717d5f17
Agwu Joseph Oguche <winjeff6@gmail.com>
2025-07-20 18:08:05 +0100

    Perform basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum_result = 10 + 5
+difference_result = 10 - 5
+product_result = 10 * 5
+
+print(f"Sum of {10} and {5} is {sum_result}")
+print(f"Difference of {10} and {5} is {difference_result}")
+print(f"Product of {10} and {5} is {product_result}")
